779b752932644a76794e97396df9dbab4bbd6122
chenhuahuiHz <huahui_c@hotmail.com>
2018-05-03 15:12:56 +0800

    3Sum Closest

--- leet_3sum.go
+++ leet_3sum.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"math"
+)
 
 //Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 //
@@ -50,7 +54,52 @@ func threeSum(arr []int)  {
 	}
 }
 
+//Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target.
+// Return the sum of the three integers. You may assume that each input would have exactly one solution.
+//
+//For example, given array S = {-1 2 1 -4}, and target = 1.
+//
+//The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+func threeSumClosest(arr []int, target int) int {
+	ret := 0
+	if len(arr) < 3 {
+		return ret
+	}
+
+	// 为了提高效率，先对arr进行升序排序
+	// 再通过两端指针往中间收缩的方式，找到合适的值
+	sort.Sort(sort.IntSlice(arr))
+
+	ret = arr[0] + arr[1] + arr[2]
+	diff := math.Abs(float64(ret-target))
+	for i:=0; i<len(arr); i++ {
+		left := i+1
+		right := len(arr)-1
+
+		for ;left<right; {
+			tmp := arr[i]+arr[left]+arr[right]
+			tmpdiff := math.Abs(float64(tmp-target))
+			if tmpdiff < diff {
+				ret = tmp
+				diff = tmpdiff
+			} else if tmp < target {
+				left++
+			} else if tmp > target {
+				right--
+			} else {
+				return tmp
+			}
+		}
+	}
+
+	return ret
+}
+
 func main()  {
-	arr := []int{-1, 0, 1, 2, -1, -4}
-	threeSum(arr)
+	//arr := []int{-1, 0, 1, 2, -1, -4}
+	//threeSum(arr)
+
+	arr := []int{-1, -4, 2, 1, -2}
+	target := 1
+	fmt.Println("arr for target:", arr, target, "is", threeSumClosest(arr, target))
 }
