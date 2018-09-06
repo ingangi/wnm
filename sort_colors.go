e80839946829521ead983af832a4f5ba0a060858
chenhuahuiHz <huahui_c@hotmail.com>
2018-09-06 14:13:54 +0800

    modify shit code

--- sort_colors.go
+++ sort_colors.go
@@ -18,39 +18,26 @@ import "fmt"
 //First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
 //Could you come up with a one-pass algorithm using only constant space?
 
-func sortColorsOnePass(colors []int)  {
-	if len(colors) == 0 {
-		return
-	}
+func sortColorsOnePass(c []int) {
 
-	// 以1为轴  0在左边  2在右边
-	left := 0
-	right := len(colors) - 1
-	fix1 := -1
-	for {
-		if colors[left] == 2 {
-			colors[left], colors[right] = colors[right], colors[left]
+	left, right := 0, len(c) - 1
+	i := 0
+	for ;i <= right; {
+		if c[i] == 0 {
+			c[i], c[left] = c[left], c[i]
+			left++
+			i++
+		} else if c[i] == 2 {
+			c[i], c[right] = c[right], c[i]
 			right--
 		} else {
-			if colors[left] == 1 && fix1 == -1 {
-				fix1 = left
-			} else if fix1 >= 0 && colors[left] == 0 {
-				colors[left], colors[fix1] = colors[fix1], colors[left]
-				left, fix1 = fix1+1, -1
-				continue
-			}
-
-			left++
-		}
-
-		if left > right {
-			break
+			i++
 		}
 	}
 }
 
 func main()  {
-	colors := []int{1,0,1,1,2,0,1}
+	colors := []int{1,1,2,1,0,1,1,2,0,1}
 	sortColorsOnePass(colors)
 	fmt.Println(colors)
 }
