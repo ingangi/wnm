c66b5e2e6a366636636e0ca2a9af7e1bde17af7d
chenhuahuiHz <huahui_c@hotmail.com>
2018-09-10 11:06:24 +0800

    Combinations

--- leet_combinations.go
+++ leet_combinations.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
 //
 //Example:
@@ -15,8 +17,31 @@ package main
 //[1,4],
 //]
 
+func combin(left []int, k int) [][]int {
+	if k == 1 {
+		tmp := make([][]int, len(left))
+		for i, v := range left {
+			tmp[i] = []int{v}
+		}
+		return tmp
+	}
+
+	c := combin(left[1:], k-1)
+	for idx, _ := range c {
+		c[idx] = append(c[idx], left[0])
+	}
+	return c
+}
+
 func combinations(n int, k int)  {
-	
+	left := make([]int, n)
+	for i:=0; i < n; i++ {
+		left[i] = i+1
+	}
+
+	for idx := 0; idx <= n-k; idx++ {
+		fmt.Println(combin(left[idx:], k))
+	}
 }
 
 func main()  {
